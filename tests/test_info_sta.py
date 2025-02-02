9aa03207c069f22f92b6f64433ddacff06527f1c
jiwon1118 <b23386585@gmail.com>
2025-02-02 21:49:07 +0900

    pytest 작성

--- tests/test_info_sta.py
+++ tests/test_info_sta.py
@@ -3,6 +3,17 @@ import pandas as pd
 import pytest
 
 def test_info():
-    df = info_sta("KT")
+    row_count = 3
+    is_asc = True
+    df = info_sta(keyword="좌석수", asc=is_asc, rcnt=row_count)
     assert isinstance(df,pd.DataFrame)
    # assert
+
+def test_info2():
+    row_count = 11
+    # When
+    df = info_sta(keyword="좌석수", asc=True, rcnt=row_count)
+    # assert
+    assert isinstance(df, pd.DataFrame) 
+    assert len(df) < row_count
+    assert df.iloc[0]["구단"] == "키움 히어로즈"
