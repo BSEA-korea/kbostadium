387ad3f605c7e699a0aeb0703a8ebed70a00101e
jiwon1118 <b23386585@gmail.com>
2025-02-08 14:36:49 +0900

    make App.py

---
+++ App.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as plt
+
+#st.set_page_config(page_title="Main", page_icon="💜")
+#st.markdown("# Main page 🐽")
+#st.sidebar.markdown("# Main page 🐽")
+
+st.title("KBO STADIUM")
+st.write("""
+information of  *kbo stadium*
+
+![img](https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160928_209%2Fdreiwunsche_1475053425909KC3LP_PNG%2Fdfafdaf.PNG&type=sc960_832)""")
